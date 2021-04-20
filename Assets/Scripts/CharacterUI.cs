747edc3b35782a32ab82686ffddd31bc58c84714
Samuel Lin <samuel990705@gmail.com>
2021-04-21 07:45:09 +0800

    RPG UI Final

---
+++ Assets/Scripts/CharacterUI.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterUI : MonoBehaviour
+{
+    GridLayoutGroup upperGrid;//Grid Layout component of stats
+    GridLayoutGroup statGrid;//Grid Layout component of stats
+
+    RectTransform upperPanel;//RectTransform of the upper panel (image and stat grid)
+    RectTransform image;//RectTransform of placeholder image of character
+
+
+    InputField[] statFields;//InputField for the 6 character stats
+
+    Text HP;
+    Text SP;
+
+    Vector2 tempVector;//used throughout code as temp variable
+
+
+    delegate void Randoms();//delegate used to ramdomize all 6 stats
+    Randoms randoms;
+    
+
+    void Start()
+    {
+        upperGrid = GameObject.FindWithTag("UpperPanel").GetComponentInChildren<GridLayoutGroup>();
+        statGrid = GameObject.FindWithTag("StatPanel").GetComponentInChildren<GridLayoutGroup>();
+
+        upperPanel = GameObject.FindWithTag("UpperPanel").GetComponentInChildren<RectTransform>();
+        image = GameObject.FindWithTag("Image").GetComponent<RectTransform>();
+
+        HP= GameObject.FindWithTag("HP").GetComponent<Text>();
+        SP = GameObject.FindWithTag("SP").GetComponent<Text>();
+
+        tempVector = new Vector2();
+
+        //fetches the InputField's of the 6 stats
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("StatField");
+        statFields = new InputField[temp.Length];
+        float zero = 0f;
+        for(int i = 0; i < temp.Length; i++)
+        {
+            statFields[i]=temp[i].GetComponent<InputField>();
+            statFields[i].text = zero.ToString();
+        }
+
+        //add all 6 random stat functions to delegate
+        randoms = randomStrength;
+        randoms += randomToughness;
+        randoms += randomDexterity;
+        randoms += randomIQ;
+        randoms += randomPower;
+        randoms += randomCharm;
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        /********************
+        *   Stat Update     *
+        * *******************/
+
+        //note negative stats are permitted (ex: debuffs)
+
+        int tempF;
+
+        //HP formula: Strength + Toughness + Power
+        //if one of the stat fields is not parse-able (null), then set to -999
+        int hp = int.TryParse(statFields[0].text, out tempF) && int.TryParse(statFields[1].text, out tempF) && int.TryParse(statFields[4].text, out tempF)
+            ? int.Parse(statFields[0].text) + int.Parse(statFields[1].text) + int.Parse(statFields[4].text)
+            : -999;
+        HP.text = "HP: " + hp.ToString();
+
+        //SP formula: Dexterity + IQ + Charm
+        //if one of the stat fields is not parse-able (null), then set to -999
+        int sp = int.TryParse(statFields[2].text, out tempF) && int.TryParse(statFields[3].text, out tempF) && int.TryParse(statFields[5].text, out tempF)
+            ? int.Parse(statFields[2].text) + int.Parse(statFields[3].text) + int.Parse(statFields[5].text)
+            : -999;
+        SP.text = "SP: " + sp.ToString();
+
+        /*********************
+         * Horizontal Layout *
+         * ******************/
+        if (Screen.width > Screen.height)
+        {
+
+            //set upper grid to have 2 column
+            upperGrid.constraintCount = 2;
+
+            //change upper grid element size
+            tempVector.x = 400;
+            tempVector.y = 300;
+            upperGrid.cellSize = tempVector;
+
+            //resize upper panel
+            tempVector.x = Screen.width;
+            tempVector.y = 311.5f;
+            upperPanel.sizeDelta = tempVector;
+
+            //set stat grid to have 2 column
+            statGrid.constraintCount = 2;
+
+            //change stat grid element size
+            tempVector.x = 200;
+            tempVector.y = 80;
+            statGrid.cellSize = tempVector;
+        }
+            
+
+        /*******************
+         * Vertical Layout *
+         * ****************/
+
+        else
+        {
+            //set upper grid to have 1 column
+            upperGrid.constraintCount = 1;
+
+            //change upper grid element size
+            tempVector.x = 400;
+            tempVector.y = 550;
+            upperGrid.cellSize = tempVector;
+
+            //resize upper panel
+            tempVector.x = Screen.width;
+            tempVector.y = 1350;
+            upperPanel.sizeDelta = tempVector;
+
+            //set stat grid to have 1 column
+            statGrid.constraintCount = 1;
+
+            //change stat grid element size
+            tempVector.x = 400;
+            tempVector.y = 80;
+            statGrid.cellSize = tempVector;
+        }
+
+    }
+
+    //set to public so it can be invoked on button click
+    public void randomize()
+    {
+
+        randoms();
+    }
+
+    void randomStrength()
+    {
+        statFields[0].text = Random.Range(1, 10).ToString();
+    }
+    void randomToughness()
+    {
+        statFields[1].text = Random.Range(1, 10).ToString();
+    }
+    void randomDexterity()
+    {
+        statFields[2].text = Random.Range(1, 10).ToString();
+    }
+    void randomIQ()
+    {
+        statFields[3].text = Random.Range(1, 10).ToString();
+    }
+    void randomPower()
+    {
+        statFields[4].text = Random.Range(1, 10).ToString();
+    }
+    void randomCharm()
+    {
+        statFields[5].text = Random.Range(1, 10).ToString();
+    }
+
+}
---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class InventoryUI : MonoBehaviour
+{
+    GridLayoutGroup invGrid;//Grid Layout component of inventory
+
+
+    RectTransform invPanel;//RectTransform of inventory panel
+    RectTransform buttonsPanel;
+    RectTransform canvas;
+
+    Item[] items;
+
+    Text[] nameTexts;
+    Text[] priceTexts;
+    Text[] weightTexts;
+    Text[] damageTexts;
+
+    Vector2 tempV;
+
+    GameObject[] tempObject;
+
+    void Start()
+    {
+        //fetches references using tags
+        canvas= GameObject.FindWithTag("Canvas").GetComponentInChildren<RectTransform>();
+        buttonsPanel= GameObject.FindWithTag("ButtonsPanel").GetComponentInChildren<RectTransform>();
+        tempV = new Vector2();
+        invGrid= GameObject.FindWithTag("InvPanel").GetComponentInChildren<GridLayoutGroup>();
+        invPanel= GameObject.FindWithTag("InvPanel").GetComponentInChildren<RectTransform>();
+
+        
+        //stores references to all the text fields of all 16 items
+        tempObject = GameObject.FindGameObjectsWithTag("Name");
+        nameTexts = new Text[tempObject.Length];
+        for (int i=0;i< tempObject.Length; i++)
+        {
+            nameTexts[i] = tempObject[i].GetComponent<Text>();
+        }
+
+        tempObject = GameObject.FindGameObjectsWithTag("Price");
+        priceTexts = new Text[tempObject.Length];
+        for (int i = 0; i < tempObject.Length; i++)
+        {
+            priceTexts[i] = tempObject[i].GetComponent<Text>();
+        }
+
+        tempObject = GameObject.FindGameObjectsWithTag("Weight");
+        weightTexts = new Text[tempObject.Length];
+        for (int i = 0; i < tempObject.Length; i++)
+        {
+            weightTexts[i] = tempObject[i].GetComponent<Text>();
+        }
+
+        tempObject = GameObject.FindGameObjectsWithTag("Damage");
+        damageTexts = new Text[tempObject.Length];
+        for (int i = 0; i < tempObject.Length; i++)
+        {
+            damageTexts[i] = tempObject[i].GetComponent<Text>();
+        }
+
+        //initialize and assign initial values for items (using values in unity editor)
+        items = new Item[tempObject.Length];
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = new Item(nameTexts[i].text, float.Parse(priceTexts[i].text), float.Parse(weightTexts[i].text), float.Parse(damageTexts[i].text));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //display item details for all 16 items
+        for(int i = 0; i < nameTexts.Length; i++)
+        {
+            nameTexts[i].text = items[i].name;
+            priceTexts[i].text = "$"+items[i].price.ToString();
+            weightTexts[i].text = items[i].weight.ToString()+"kg";
+            damageTexts[i].text = items[i].damage.ToString() + " dmg";
+        }
+
+        //adjust button panel
+        tempV.x = buttonsPanel.sizeDelta.x;
+        tempV.y = canvas.sizeDelta.y*0.15f;
+        buttonsPanel.sizeDelta = tempV;//update size
+
+        //adjust inventory panel
+        tempV = canvas.sizeDelta;
+        tempV.y *= 0.7f;
+        invPanel.sizeDelta = tempV;//update size
+
+        tempV.x = 0;
+        tempV.y= canvas.sizeDelta.y * -0.35f;
+        invPanel.localPosition = tempV;//update position
+
+
+        /*********************
+         * Horizontal Layout *
+         * ******************/
+        if (Screen.width > Screen.height)
+        {
+
+            //set inventory grid to have 8 column
+            invGrid.constraintCount = 8;
+        }
+
+
+        /*******************
+         * Vertical Layout *
+         * ****************/
+
+        else
+        {
+            //set inventory grid to have 4 column
+            invGrid.constraintCount = 4;
+        }
+
+        //scales cell size
+        tempV.x = invPanel.sizeDelta.x / invGrid.constraintCount;
+        tempV.y = invPanel.sizeDelta.y / (16 / invGrid.constraintCount);//there are 16 cells
+        invGrid.cellSize = tempV;
+
+    }
+
+
+    //4 sort functions that uses LINQ
+    public void sortByName()
+    {
+        //sort objects using LINQ
+        Item[] sorted = items.OrderBy(i => i.name).ToArray();
+        items = sorted;//set original array to sorted array
+    }
+    public void sortByPrice()
+    {
+        //sort objects using LINQ
+        Item[] sorted = items.OrderBy(i => i.price).ToArray();
+        items = sorted;//set original array to sorted array
+    }
+    public void sortByWeight()
+    {
+        //sort objects using LINQ
+        Item[] sorted = items.OrderBy(i => i.weight).ToArray();
+        items = sorted;//set original array to sorted array
+    }
+    public void sortByDamage()
+    {
+        //sort objects using LINQ
+        Item[] sorted = items.OrderBy(i => i.damage).ToArray();
+        items = sorted;//set original array to sorted array
+    }
+}
+
+
+//Item object definition 
+public class Item
+{
+    public string name;
+    public float weight;
+    public float price;
+    public float damage;
+
+    //constructor
+    public Item(string name, float price, float weight, float damage)
+    {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+        this.damage = damage;
+    }
+
+
+}
+
+//name
+//price
+//weight
+//damage
